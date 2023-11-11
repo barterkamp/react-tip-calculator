78a9324bd25ca0811bf3f6cf64b2c0417b9149ff
Bart Erkamp <bart.erkamp@maxserv.com>
2023-11-11 18:30:40 +0100

    add reset button with reset functionality to output component

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,28 +1,51 @@
 import React from 'react';
 
-function Output({ tipAmount, totalAmount }) {
+function Output({
+  tipAmount,
+  totalAmount,
+  setSelectedButtonId,
+  reset,
+  setReset,
+  setBill,
+  setNumPeople,
+  setCustom,
+  setError,
+}) {
+  const handleReset = () => {
+    setReset(false);
+    setBill('');
+    setNumPeople('');
+    setCustom('');
+    setError(false);
+    setSelectedButtonId(null);
+  };
+
   return (
-    <div className="bg-darkGreen rounded-2xl pt-10 px-6 pb-6">
+    <div className="flex flex-col bg-darkGreen rounded-2xl pt-10 px-6 pb-6 md:min-h-full md:p-10">
       <div className="flex justify-between items-center mb-6">
         <div>
           <p className="text-white text-base font-spaceMono">Tip amount</p>
           <span className="block text-sm text-lightGray font-spaceMono">/ person</span>
         </div>
         <div>
-          <p className="text-3xl text-mediumGreen font-spaceMono">${tipAmount}</p>
+          <p className="text-3xl text-mediumGreen font-spaceMono md:text-5xl">${tipAmount}</p>
         </div>
       </div>
-
       <div className="flex justify-between items-center mb-8">
         <div>
-          <p className="text-white text-base font-spaceMono">Tip amount</p>
+          <p className="text-white text-base font-spaceMono">Total amount</p>
           <span className="block text-sm text-lightGray font-spaceMono">/ person</span>
         </div>
         <div>
-          <p className="text-3xl text-mediumGreen font-spaceMono">${totalAmount}</p>
+          <p className="text-3xl text-mediumGreen font-spaceMono md:text-5xl">${totalAmount}</p>
         </div>
       </div>
-      <button className="bg-mediumGreen text-center font-spaceMono text-xl text-darkGreen uppercase w-full py-2.5 rounded-md">
+      <button
+        onClick={handleReset}
+        className={`${
+          reset ? 'bg-mediumGreen' : 'bg-[#0D686D]'
+        } text-center font-spaceMono text-xl text-darkGreen uppercase w-full py-2.5 rounded-md md:mt-auto`}
+      >
         reset
       </button>
     </div>
