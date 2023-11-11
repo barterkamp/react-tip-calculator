646559b1ee89b827eb782ccc6800acd5e71398c9
Bart Erkamp <bart.erkamp@maxserv.com>
2023-11-11 18:35:44 +0100

    add button background when specific button is selected

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Buttons({ setPercent, setCustom }) {
+function Buttons({ setPercent, setCustom, selectedButtonId, setSelectedButtonId }) {
   const buttons = [
     { id: 1, percentage: 5 },
     { id: 2, percentage: 10 },
@@ -9,23 +9,30 @@ function Buttons({ setPercent, setCustom }) {
     { id: 5, percentage: 50 },
   ];
 
+  const handleButtonId = (id) => {
+    setSelectedButtonId(id);
+  };
+
+  // als de button geklikt wordt, active state op die specifieke button plaatsen met setActive = true die de bg-color naar mediumGreen zet
   return (
     <>
       {buttons.map((button) => (
-        <React.Fragment key={button.id}>
-          <button
-            onClick={() => {
-              setPercent(button.percentage);
-              if (setPercent) {
-                setCustom('');
-              }
-            }}
-            className="bg-darkGreen text-white text-2xl font-spaceMono rounded-md py-1.5 w-auto hover:bg-mediumGreen
-            hover:text-darkGreen focus:bg-mediumGreen focus:text-darkGreen active:bg-mediumGreen active:text-mediumGreen"
-          >
-            {button.percentage}%
-          </button>
-        </React.Fragment>
+        <button
+          key={button.id}
+          onClick={() => {
+            setPercent(button.percentage);
+            handleButtonId(button.id);
+            if (setPercent) {
+              setCustom('');
+            }
+          }}
+          className={`${
+            selectedButtonId === button.id ? 'bg-mediumGreen text-darkGreen' : 'bg-darkGreen text-white'
+          } bg-darkGreen text-2xl font-spaceMono rounded-md py-1.5 w-auto hover:bg-mediumGreen
+            hover:text-darkGreen focus:bg-mediumGreen focus:text-darkGreen active:bg-mediumGreen active:text-darkGreen`}
+        >
+          {button.percentage}%
+        </button>
       ))}
     </>
   );
